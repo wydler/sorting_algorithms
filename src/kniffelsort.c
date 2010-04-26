38c8e6d6a8e76b670e6025cc5bbf071aa306a61e
Michael Wydler <michael.wydler@gmail.com>
2010-04-26 14:55:04 +0200

    new algorithem. change position of the elemens random

---
+++ src/kniffelsort.c
@@ -0,0 +1,37 @@
+/*
+ * kniffelsort.cpp
+ *
+ *  Created on: 26.04.2010
+ *      Author: michael
+ */
+
+#include "kniffelsort.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void kniffelsort(int a[], int l) {
+	int i;
+	int count = 0;
+	while(1) {
+		count++;
+		int flag = 0;
+		for(i = 0; i < l; i++) {
+			int tmp = a[i];
+			int rand2 = rand()%l;
+			a[i] = a[rand2];
+			a[rand2] = tmp;
+		}
+		for(i = 0; i < l-1; i++) {
+			if(a[i] > a[i+1]) {
+				flag++;
+			}
+		}
+		if(flag == 0) {
+			return 1;
+		}
+		if(count%10000000 == 0) {
+			printf(" %d\n", count);
+		}
+	}
+}
---
+++ src/kniffelsort.h
@@ -0,0 +1,13 @@
+/*
+ * kniffelsort.h
+ *
+ *  Created on: 26.04.2010
+ *      Author: michael
+ */
+
+#ifndef KNIFFELSORT_H_
+#define KNIFFELSORT_H_
+
+void kniffelsort(int[], int);
+
+#endif /* KNIFFELSORT_H_ */
